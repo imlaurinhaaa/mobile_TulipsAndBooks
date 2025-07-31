61aa41d3a000c397fe29e85479cf3b25608f6f4e
Evelyn <evelynoliveira.tds1.senai@gmail.com>
2025-07-31 11:32:48 -0300

    feat: create page login and cadastro

--- navigation/StackNavigator.js
+++ navigation/StackNavigator.js
@@ -3,15 +3,19 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "../pages/Login";
 import Cadastro from "../pages/Cadastro";
 import DrawerNavigator from "./DrawerNavigator";
+import Home from "../pages/Home";
+import Feed from "../pages/Feed"; 
 
 const Stack = createNativeStackNavigator();
 
 export default function StackNavigator() {
     return (
-        <Stack.Navigator initialRouteName="Login">
+        <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Home" component={Home} />
             <Stack.Screen name="Login" component={Login} />
             <Stack.Screen name="Cadastro" component={Cadastro} />
-            <Stack.Screen name="Home" component={DrawerNavigator} />
+            <Stack.Screen name="Feed" component={Feed} />
+            <Stack.Screen name="Drawer" component={DrawerNavigator} />
         </Stack.Navigator>
     );
 }
